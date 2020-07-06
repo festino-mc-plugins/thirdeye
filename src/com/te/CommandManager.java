9e367b385e0bc0299805159cb4c697a99888175c
Festino <offestashka@mail.ru>
2020-07-06 21:50:40 +0300

    Added "/fix walls" to fix walls on 1.16

--- src/com/te/CommandManager.java
+++ src/com/te/CommandManager.java
@@ -6,11 +6,14 @@ import java.text.NumberFormat;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.Server;
+import org.bukkit.World;
 import org.bukkit.attribute.Attribute;
+import org.bukkit.block.Block;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -256,6 +259,11 @@ public class CommandManager implements CommandExecutor {
 		return false;
 	}
 	
+	public boolean isWall(Material m) {
+		// is switch faster?
+		return m.toString().endsWith("WALL");
+	}
+	
 	public boolean onFixBranch(CommandSender sender, String[] args)
 	{
 		if(args.length > 1)
@@ -299,25 +307,62 @@ public class CommandManager implements CommandExecutor {
 			}
 					
 			Material m;
-			if(args[0].equalsIgnoreCase("lava")) m = Material.LAVA; 
-			else if(args[0].equalsIgnoreCase("water")) m = Material.WATER;
-			else
-			{
-				sender.sendMessage(ChatColor.RED + "Использование: /fix <lava/water> <r>");
+			if (args[0].equalsIgnoreCase("lava")) {
+				m = Material.LAVA; 
+			} else if (args[0].equalsIgnoreCase("water")) {
+				m = Material.WATER;
+			} else if (args[0].equalsIgnoreCase("walls")) {
+				m = Material.ANDESITE_WALL;
+			} else {
+				sender.sendMessage(ChatColor.RED + "Использование: /fix <lava/water/walls> <r>");
 				return false;
 			}
-			double r = Double.parseDouble(args[1]);
-			for(int i=(int)-r;i<=r;i++){
-				for(int j=(int)-r;j<=r;j++){
-					for(int o=(int)-r;o<=r;o++){
-						if(((Player)sender).getWorld().getBlockAt((int)Math.ceil(((Player)sender).getLocation().getX()+i), (int)Math.ceil(((Player)sender).getLocation().getY()+j), (int)Math.ceil(((Player)sender).getLocation().getZ()+o)).getType() == m)
-							((Player)sender).getWorld().getBlockAt((int)Math.ceil(((Player)sender).getLocation().getX()+i), (int)Math.ceil(((Player)sender).getLocation().getY()+j), (int)Math.ceil(((Player)sender).getLocation().getZ()+o)).setType(Material.AIR);
+			
+			if (m == Material.WATER || m == Material.LAVA) {
+				double r = Double.parseDouble(args[1]);
+				Player player = (Player) sender;
+				int x0 = player.getLocation().getBlockX();
+				int y0 = player.getLocation().getBlockY();
+				int z0 = player.getLocation().getBlockZ();
+				World world = player.getWorld();
+				for (int i = (int)-r; i <= r; i++) {
+					for (int j = (int)-r; j <= r; j++) {
+						for (int k = (int)-r; k <= r; k++) {
+							Block block = world.getBlockAt(x0 + i, y0 + j, z0 + k);
+							if (block.getType() == m)
+								block.setType(Material.AIR);
+						}
+					}
+				}
+			} else if (m == Material.ANDESITE_WALL) {
+				double r = Double.parseDouble(args[1]);
+				Player player = (Player) sender;
+				int x0 = player.getLocation().getBlockX();
+				int y0 = player.getLocation().getBlockY();
+				int z0 = player.getLocation().getBlockZ();
+				World world = player.getWorld();
+				for (int i = (int)-r; i <= r; i++) {
+					for (int j = (int)-r; j <= r; j++) {
+						for (int k = (int)-r; k <= r; k++) {
+							Block block = world.getBlockAt(x0 + i, y0 + j, z0 + k);
+							if (isWall(block.getType())) {
+								/*m = block.getType();
+								block.setType(Material.AIR);
+								block.setType(m); // breaks walls
+								block.getState().update(); // no effect*/
+								// Wall wall = (Wall) block.getBlockData();
+								String wallName = block.getType().toString().toLowerCase();
+								String setblockCommand = String.format("setblock %d %d %d %s", x0 + i, y0 + j, z0 + k, wallName);
+								Bukkit.dispatchCommand(sender, setblockCommand);
+							}
+						}
 					}
 				}
 			}
 			return true;
 		}
-		sender.sendMessage(ChatColor.RED + "Использование: /fix <lava/water> <r>");
+		sender.sendMessage(ChatColor.RED + "Использование: /fix <lava/water> <r> - убирает лаву/воду");
+		sender.sendMessage(ChatColor.RED + "Использование: /fix <walls> <r> - чинит ограды на 1.16");
 		sender.sendMessage(ChatColor.RED + "Использование: /fix ms <nickname> [value] - скорость бега");
 		return false;
 	}
